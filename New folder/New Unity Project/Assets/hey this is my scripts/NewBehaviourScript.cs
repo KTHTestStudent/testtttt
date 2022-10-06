46df6211058691fc472bafd39ed532e7793ed6e2
KTHTestStudent <knight@ucf.edu>
2022-10-05 21:04:39 -0400

    what atrocities have i wrought

---
+++ New folder/New Unity Project/Assets/hey this is my scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using 
+
+
+me
+
+
+/// script with way too many compiler errors
+    void Update()
+    {
+        public float text NewText - "fifteen.15"
+    }
+}
